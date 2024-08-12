d066950d285ec97d313a8f2a701dfdeecde4c604
nervewastaken <krishverma2004@gmail.com>
2024-08-12 09:46:51 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  //backend
+
+  const gay = async () => {
+
+    try {
+    } catch (err) {
+      console.log(err);
+    } finally {
+    }
+  };
+
+  const [stuff, alsostuff] = useState(1);
+
+
+
+  //frontend |
+  return (
+    <main className="">
+      <div className="flex justify-center items-center">gay</div>
+    </main>
+  );
+}
